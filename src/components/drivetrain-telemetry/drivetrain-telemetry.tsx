4f896db0d773418597034d5745157703d9dbdd6f
Erzhan <kk.erzhan@gmail.com>
2020-02-25 13:41:14 +0600

    Finished drivetrain design

--- src/components/drivetrain-telemetry/drivetrain-telemetry.tsx
+++ src/components/drivetrain-telemetry/drivetrain-telemetry.tsx
@@ -8,24 +8,27 @@ import { Component, h, getAssetPath } from '@stencil/core';
 })
 export class DrivetrainTelemetry {
   getStyle(): any {
-    return {'background-image': `url(${getAssetPath('./assets/drivetrain-svg.svg')}`}
+    return { 'background-image': `url(${getAssetPath('./assets/drivetrain-svg.svg')}` }
   }
 
   render() {
     return (
-      <div class='container' style={this.getStyle()}>
-        <div class='front-wheels'>
-          <spark-max-info-small />
-          <spark-max-info-small alignRight />
-        </div>
-        <div class='middle-wheels'>
-          <spark-max-info-small />
-          <gyro-info-small />
-          <spark-max-info-small alignRight />
-        </div>
-        <div class='back-wheels'>
-          <spark-max-info-small />
-          <spark-max-info-small alignRight />
+      <div class='telemetry-main'>
+        <p id='title'>Drivetrain</p>
+        <div class='container' style={this.getStyle()}>
+          <div class='front-wheels'>
+            <spark-max-info-small />
+            <spark-max-info-small alignRight />
+          </div>
+          <div class='middle-wheels'>
+            <spark-max-info-small />
+            <gyro-info-small />
+            <spark-max-info-small alignRight />
+          </div>
+          <div class='back-wheels'>
+            <spark-max-info-small />
+            <spark-max-info-small alignRight />
+          </div>
         </div>
       </div>
     );
